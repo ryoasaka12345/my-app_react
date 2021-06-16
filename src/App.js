5e383d967cba545208df4a3a19e8ad0e7ae8815e
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-16 15:25:48 +0900

    Add javascript logic to component 	modified:   src/App.js 	modified:   src/components/ProductItem.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,43 @@ import './components/ProductItem.css'
 
 // The syntax inside function App() called JSX
 function App() {
+
+  const products = [
+    { name: "T-Shirt", category: "Fashion", price: 12 },
+    { name: "Red Dress", category: "Fashion", price: 20 },
+    { name: "iPhoneX", category: "Smartphpne", price: 600 },
+    { name: "iPhone 12", category: "Fasion", price: 1200 },
+    { name: "Smar Tivi Sony 59 Inch", category: "Smart Tivi", price: 700 }
+  ];
+
   return (
     <div className="App">
       <h2>Getting Started with React.</h2>
-      <ul>
-        <ProductItem></ProductItem>
-      </ul>
+      <ProductItem
+        name={products[0].name}
+        price={products[0].price}
+        category={products[0].category}
+      ></ProductItem>
+      <ProductItem
+        name={products[1].name}
+        price={products[1].price}
+        category={products[1].category}
+      ></ProductItem>
+      <ProductItem
+        name={products[2].name}
+        price={products[2].price}
+        category={products[2].category}
+      ></ProductItem>
+      <ProductItem
+        name={products[3].name}
+        price={products[3].price}
+        category={products[3].category}
+      ></ProductItem>
+      <ProductItem
+        name={products[4].name}
+        price={products[4].price}
+        category={products[4].category}
+      ></ProductItem>
     </div>
   );
 }
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,10 +1,23 @@
-function ProductItem(){
+import './ProductItem.css';
+
+function ProductItem(props){
+/*     const category = "Fshion";
+    const productName = "T-Shirt";
+    const price = "$12";
+    const product = [
+        {name: "T-Shirt", category: "Fashion", price: 12},
+        {name: "Red Dress", category: "Fashion", price: 20},
+        {name: "iPhoneX", category: "Smartphpne", price: 600},
+        {name: "iPhone 12", category: "Fasion", price: 1200},
+        {name: "Smar Tivi Sony 59 Inch", category: "Smart Tivi", price: 700}
+    ]; */
+
     return(
         <div className="product-item">
-            <div>Fashion</div>
+            <div>{props.category}</div>
             <div className="product-item__description">
-                <h3>T-Shirt</h3>
-                <div className="product-item__price">$12</div>
+                <h3>{props.name}</h3>
+                <div className="product-item__price">{props.price}</div>
             </div>
         </div>    
     );
