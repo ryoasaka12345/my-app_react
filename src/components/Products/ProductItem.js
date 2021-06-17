d0f852c44ec0e7436d09dcde03675871778d1350
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-17 10:59:28 +0900

    Working with state 	modified:   src/components/Products/ProductItem.js

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,18 +1,22 @@
+import React, { useState } from 'react';
 import './ProductItem.css';
 
 function ProductItem(props) {
-    // Vanilla Javascript
     const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(props.price)
 
+    // `useState` return an array with 2 elements, 
+    // first is the value, second is a function
+    const [name, setName] = useState(props.name);
+
     const titleClickHandler = () => {
-        alert('Clicked on Title!');
+        alert(name + ' clicked');
     }
 
     return (
         <div className="product-item">
             <div>{props.category}</div>
             <div className="product-item__description">
-                <h3 onClick={titleClickHandler}>{props.name}</h3>
+                <h3 onClick={titleClickHandler}>{name}</h3>
                 <div className="product-item__price">{price}</div>
             </div>
         </div>
