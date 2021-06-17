26a2216f843a24de937bbccba99882eaad74c3a3
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-17 15:16:09 +0900

    Handle Events and Form Data 	modified:   src/components/Products/NewProduct.css 	modified:   src/components/Products/NewProduct.js

--- src/components/Products/NewProduct.js
+++ src/components/Products/NewProduct.js
@@ -1,22 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewProduct.css';
 
 const NewProduct = () => {
+    // Define states for user input data
+    //     The first argument in [] is the name of the box, 
+    //     and the second is the function used to put the value into the box.
+    const [inputedName, setInputedName] = useState('');
+    const [inputedPrice, setInputedPrice] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    // Define handle functions for form events.
+    // e: event
+    // vanila javascript: function OnChange() {code here}
+    const nameChangeHandler = (e) => {
+        setInputedName(e.target.value); // Put `e.target.value` into the box named `inputedName`
+    }
+    const priceChangeHandler = (e) => {
+        setInputedPrice(e.target.value);
+    }
+    const categoryChangeHandler = (e) => {
+        setSelectedCategory(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log(inputedName, inputedPrice, selectedCategory);
+
+        // reset form to empty
+
+        setInputedName('');
+        setInputedPrice('');
+        setSelectedCategory('');
+    }
+
     return (
         <div className='new-product'>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='new-product__controls'>
                     <div className='new-product__control'>
                         <label>name</label>
-                        <input type='text' placeholder="Product name" />
+                        <input
+                            onChange={nameChangeHandler} // vanila javascript: onChange="nameChangeHandler"
+                            value={inputedName}
+                            type='text'
+                            placeholder="Product name"
+                        />
                     </div>
                     <div className='new-product__control'>
                         <label>Price</label>
-                        <input type='number' placeholder="Product price" />
+                        <input
+                            onChange={priceChangeHandler}
+                            value={inputedPrice}
+                            type='number'
+                            placeholder="Product price"
+                        />
                     </div>
                     <div className='new-product__control'>
                         <label>category</label>
-                        <select>
+                        <select
+                            value={selectedCategory}
+                            onChange={categoryChangeHandler}>
                             <option value="Fashion">Fashion</option>
                             <option value="Smart Phone">Smartphone</option>
                             <option value="Fashion">Smart Tivi</option>
