80fc599b93b40558294d1b3b50ba7f239e920427
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-17 10:27:50 +0900

    Handling event in React Component 	modified:   src/components/Products/ProductItem.js

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,17 +1,21 @@
 import './ProductItem.css';
 
-function ProductItem(props){
+function ProductItem(props) {
     // Vanilla Javascript
-    const price = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(props.price)
+    const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(props.price)
 
-    return(
+    const titleClickHandler = () => {
+        alert('Clicked on Title!');
+    }
+
+    return (
         <div className="product-item">
             <div>{props.category}</div>
             <div className="product-item__description">
-                <h3>{props.name}</h3>
+                <h3 onClick={titleClickHandler}>{props.name}</h3>
                 <div className="product-item__price">{price}</div>
             </div>
-        </div>    
+        </div>
     );
 }
 
