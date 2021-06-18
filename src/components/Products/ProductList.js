36fc4f9b81ccf007fccc651a8dffaab698a08b2a
ryoasaka12345 <ryo.asaka@g.pascalia.co.jp>
2021-06-18 11:40:17 +0900

    Rendering Lists in React 	modified:   src/components/Products/ProductList.js

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -5,31 +5,14 @@ import List from '../UI/Lists';
 function ProductList(props) {
     return (
         <List>
-            <ProductItem
-                name={props.products[0].name}
-                price={props.products[0].price}
-                category={props.products[0].category}
-            ></ProductItem>
-            <ProductItem
-                name={props.products[1].name}
-                price={props.products[1].price}
-                category={props.products[1].category}
-            ></ProductItem>
-            <ProductItem
-                name={props.products[2].name}
-                price={props.products[2].price}
-                category={props.products[2].category}
-            ></ProductItem>
-            <ProductItem
-                name={props.products[3].name}
-                price={props.products[3].price}
-                category={props.products[3].category}
-            ></ProductItem>
-            <ProductItem
-                name={props.products[4].name}
-                price={props.products[4].price}
-                category={props.products[4].category}
-            ></ProductItem>
+            {/* like `foreach($products as $product)  */}
+            {props.products.map(product => (
+                <ProductItem
+                    name={product.name}
+                    price={product.price}
+                    category={product.category}
+                />
+            ))}
         </List>
     );
 }
